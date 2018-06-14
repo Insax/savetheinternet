e3a1634d685b96bacc5f9ffcc4f6a818a7b315a7
aytic <me@lynx.pub>
2018-06-14 18:07:29 +0200

    added typewriter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+"use strict";
 import 'bootstrap';
 import ResponsiveVideo from './components/responsive-video';
 import Typed from 'typed.js';
--- assets/js/pages/landing.js
+++ assets/js/pages/landing.js
@@ -1,2 +1,3 @@
 import './landing/countdown.js';
 import "./landing/tweets";
+import './landing/typer.js';
