691a19a251181b2a6439f1bb83c97473ea0dd22d
ThermalCube <nikolai@leesker.de>
2018-06-14 16:40:16 +0200

    [JS] Added Typed.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
 import 'bootstrap';
 import ResponsiveVideo from './components/responsive-video';
+import Typed from 'typed.js';
+
+//Comment für @Inf4m0u5
+// (function () {
+// 	'use strict';
+// 	var t = Typed;
+// 	debugger;
+// })();
 
 (function($) {
 	new ResponsiveVideo('.responsive-video').activate();
