d0716b914b745736e6b9aba7a29b20d14d52ded4
Tim Windelschmidt <fionera@fionera.de>
2018-06-14 20:14:04 +0000

    Rename default Language

--- config/routes.php
+++ config/routes.php
@@ -6,7 +6,7 @@ use Symfony\Component\Routing\RouteCollection;
 $routes = new RouteCollection();
 $routes->add('index', new Route('/{_locale}', array(
     '_controller' => 'App\\Controller\\IndexController::index',
-    '_locale' => 'en',
+    '_locale' => 'en_GB',
 ), array(
     '_locale' => implode('|', \App\EventSubscriber\getAvailableLanguages()),
 )));
