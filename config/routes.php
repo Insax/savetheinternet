5687a43108c62cebec32bdc1e5d015133ebe9b1c
Tim Windelschmidt <fionera@fionera.de>
2018-06-15 20:36:02 +0000

    Fix Translation on Routes and add correct Links
    Please learn to use path()

--- config/routes.php
+++ config/routes.php
@@ -11,21 +11,21 @@ $routes->add('index', new Route('/{_locale}', array(
     '_locale' => implode('|', \App\EventSubscriber\getAvailableLanguages()),
 )));
 
-$routes->add('imprint', new Route('/imprint', array(
+$routes->add('imprint', new Route('/{_locale}/imprint', array(
     '_controller' => 'App\\Controller\\IndexController::imprint',
     '_locale' => 'en_GB',
 ), array(
     '_locale' => implode('|', \App\EventSubscriber\getAvailableLanguages()),
 )));
 
-$routes->add('privacy', new Route('/privacy', array(
+$routes->add('privacy', new Route('/{_locale}/privacy', array(
     '_controller' => 'App\\Controller\\IndexController::privacy',
     '_locale' => 'en_GB',
 ), array(
     '_locale' => implode('|', \App\EventSubscriber\getAvailableLanguages()),
 )));
 
-$routes->add('about', new Route('/about', array(
+$routes->add('about', new Route('/{_locale}/about', array(
     '_controller' => 'App\\Controller\\IndexController::about',
     '_locale' => 'en_GB',
 ), array(
