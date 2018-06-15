f592991da3f04e917e151248bced1263ef881250
aytic <me@lynx.pub>
2018-06-15 10:31:59 +0200

    tweetjs responsive "fix"

--- assets/js/pages/landing/tweets.js
+++ assets/js/pages/landing/tweets.js
@@ -6,7 +6,7 @@ function makeTemplate(element){
            '<div class="post-right">'+
            '<div class="post-header">'+
            '<div class="profile-name">'+
-           '<span class="profile-displayname">' + element.user.name + '</span>'+
+           //'<span class="profile-displayname">' + element.user.name + '</span>'+
            '<span class="profile-username"><a href="https://twitter.com/' + element.user.screen_name + '">@' + element.user.screen_name + '</a></span>'+
            '<span class="post-time"> ' + moment(new Date(element.created_at)).fromNow() + '</span>'+
            '</div>'+
