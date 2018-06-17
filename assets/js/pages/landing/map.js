a745affa6ceb35937ab5549bae0e7f8cc60f85f4
Florian Maak <fmaak@outlook.de>
2018-06-17 14:46:09 +0200

    Added german translation

--- assets/js/pages/landing/map.js
+++ assets/js/pages/landing/map.js
@@ -1,6 +1,5 @@
 export default class Map {
     constructor() {
-        this.map = null;
         this.mapContainer = document.getElementById('map');
         this.frameContainer = this.mapContainer.getElementsByTagName('iframe')[0];
 
@@ -8,7 +7,6 @@ export default class Map {
     }
 
     loadMap() {
-        this.frameContainer.src = `https://www.google.com/maps/d/u/0/embed?mid=11FTfbx7UKU_MznD8wevgqnSTMII8_hjS&ll=51.631131202713426%2C9.005876179687448&z=7`;
         this.frameContainer.onload = () => this.mapContainer.classList.remove('map-loader');
     }
 }
